731da1f3a859961fe939081d2a7b86eace4673a1
Zhao Wang <zhao@platoteam.com>
2021-02-10 11:31:41 -0800

    initial code commit, draw a blue background now

---
+++ main.cpp
@@ -0,0 +1,91 @@
+#define GLFW_INCLUDE_NONE
+#include "windows.h"
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+        {
+                { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+                {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+                {   0.f,  0.6f, 0.f, 0.f, 1.f }
+        };
+
+static const char* vertex_shader_text =
+        "#version 110\n"
+        "uniform mat4 MVP;\n"
+        "attribute vec3 vCol;\n"
+        "attribute vec2 vPos;\n"
+        "varying vec3 color;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+        "    color = vCol;\n"
+        "}\n";
+
+static const char* fragment_shader_text =
+        "#version 110\n"
+        "varying vec3 color;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_FragColor = vec4(color, 1.0);\n"
+        "}\n";
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main() {
+    GLFWwindow* window;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit()) {
+        std::cerr << "Could not init GLFW\n";
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(640, 480, "Test", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Could not create window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwMakeContextCurrent(window);
+
+    // rest of code goes here
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0, 0, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
